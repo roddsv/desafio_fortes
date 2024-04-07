5870c48072bf6bab0b24fd65738a70dbf3a654f6
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2024-04-06 22:08:53 -0300

    Form stylization - part 02 - padding and bg color

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -6,12 +6,26 @@ const LoginForm: React.FC = () => {
     const [password, setPassword] = useState<string>('');
 
     return (
-                    <main>
-                <form>
-                    <p className="login-header">Faça seu login</p>
-                </form>
-            </main>
-        
+        <main>
+            <form>
+                <p className="login-header">Faça seu login</p>
+                <div>
+                    <input 
+                    type="text" 
+                    placeholder="Digite o nome de usuário" 
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+                <div>
+                    <input 
+                    type="password" 
+                    placeholder="Senha" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+            </form>
+        </main>
+
     );
 };
 
