edf11205978f8924c7e720c6bebc58a141ff6b97
Rodrigo Soares <rodrigo.soaresvalente@ibm.com>
2024-04-06 22:27:07 -0300

    Form stylization - part 05 - Input field and field stylization

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -11,14 +11,16 @@ const LoginForm: React.FC = () => {
                 <p className="login-header">Faça seu login</p>
                 <div className="field">
                     <input 
-                    type="text" 
-                    placeholder="Digite o nome de usuário" 
+                    type="text"
+                    className="input-field"
+                    placeholder="Usuário" 
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}/>
                 </div>
                 <div className="field">
                     <input 
-                    type="password" 
+                    type="password"
+                    className="input-field"
                     placeholder="Senha" 
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}/>
